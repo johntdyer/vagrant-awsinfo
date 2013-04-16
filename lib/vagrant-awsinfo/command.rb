6e6e223a69efb443acce99b1eb076c395b9886ed
John Dyer <jdyer@voxeolabs.com>
2013-04-16 17:03:45 -0400

    missing end from module

--- lib/vagrant-awsinfo/command.rb
+++ lib/vagrant-awsinfo/command.rb
@@ -1,29 +1,30 @@
 module VagrantAwsInfo
 
-class Command < Vagrant.plugin(2, :command)
+    class Command < Vagrant.plugin(2, :command)
 
-    Settings = Struct.new(:machines)
+        Settings = Struct.new(:machines)
 
-    def execute
+        def execute
 
-        require "optparse"
+            require "optparse"
 
-        settings = Settings.new
-        settings.machines = []
+            settings = Settings.new
+            settings.machines = []
 
-        options = OptionParser.new do |o|
-            o.banner = "Return SSH info from an AWS provisioned Vagrant machine"
-            o.on("-m MACHINE", "The machine to query.") do |value|
-                settings.machines << value
+            options = OptionParser.new do |o|
+                o.banner = "Return SSH info from an AWS provisioned Vagrant machine"
+                o.on("-m MACHINE", "The machine to query.") do |value|
+                    settings.machines << value
+                end
             end
-        end
-        settings.machines = ["default"] if settings.machines.empty?
+            settings.machines = ["default"] if settings.machines.empty?
 
-        with_target_vms(settings.machines) do |machine|
-            if machine.provider_name == :aws
-                @env.ui.info machine.provider.ssh_info.to_json
-            else
-                @env.ui.error "Sorry this plugin currently only supports the AWS provider"
+            with_target_vms(settings.machines) do |machine|
+                if machine.provider_name == :aws
+                    @env.ui.info machine.provider.ssh_info.to_json
+                else
+                    @env.ui.error "Sorry this plugin currently only supports the AWS provider"
+                end
             end
         end
     end
